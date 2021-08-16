eb6e7c9ac1596e5903ebdaf729a057330b62f198
Ruslan Guryanov <ruslan.guryanov@oliveai.com>
2021-08-16 16:14:07 -0400

    HELPS-1051: RG - adding an example for the OCR

--- ldk/javascript/examples/ocr-loop/whisperManager/index.ts
+++ ldk/javascript/examples/ocr-loop/whisperManager/index.ts
@@ -45,37 +45,78 @@ export const emitOcrRequestWhisper = async (): Promise<void> => {
               const boundingBoxType = onClickWhisper.componentState.get(
                 boundingBoxTypeId,
               ) as number;
-              console.info(boundingBoxType);
+              console.info(`Selected boundingBoxType: ${boundingBoxType}`);
               if (boundingBoxType === undefined || boundingBoxType < 0 || boundingBoxType > 4) {
                 (components[0] as Select).validationError = 'Please select Bounding Box Type.';
                 onClickWhisper.update({ components });
                 return;
               }
               (components[0] as Select).validationError = null;
-              onClickWhisper.update({ components });
-              try {
-                const ocrResults = await processOcr(boundingBoxType);
-                ocrResults.forEach((ocrResult) => {
-                  console.info(
-                    `Found text: ${ocrResult.text}, with confidence: ${ocrResult.confidence} per your request.`,
-                  );
-                });
-              } catch (error) {
-                console.error(error);
-              }
+              onClickWhisper.update({
+                components: [
+                  {
+                    type: WhisperComponentType.Markdown,
+                    body: `Please see console for results. **Please be patient!** ;)`,
+                  },
+                  {
+                    type: WhisperComponentType.Box,
+                    direction: Direction.Horizontal,
+                    justifyContent: JustifyContent.Right,
+                    children: [
+                      {
+                        type: WhisperComponentType.Button,
+                        label: 'Restart',
+                        onClick: (error: Error, onClickWhisper: Whisper) => {
+                          if (error) {
+                            console.error(error);
+
+                            return;
+                          }
+
+                          onClickWhisper.close((error: Error) => {
+                            if (error) {
+                              console.error(error);
+
+                              return;
+                            }
+                          });
+
+                          createNewWhisper(components);
+                        },
+                      },
+                    ],
+                  },
+                ],
+              });
+              var start = new Date().getTime();
+              console.info(`Starting OCR process`);
+              const ocrResults = await processOcr(boundingBoxType);
+              var end = new Date().getTime();
+              console.info(`Ending OCR process`);
+              console.info(`OCR process time: ${(end - start)/1000} sec`);
+
+              ocrResults.forEach((ocrResult) => {
+                console.info(
+                  `Found text: ${ocrResult.text}, with confidence: ${ocrResult.confidence} per your request.`,
+                );
+              });
             },
           },
         ],
       },
     ];
-    await whisper.create({
-      label: 'Please provide OCR criteria',
-      onClose: () => {
-        // do nothing.
-      },
-      components,
-    });
+    createNewWhisper(components);
   } catch (error) {
     console.error(error);
   }
+};
+
+const createNewWhisper = async (components: Component[]) => {
+  await whisper.create({
+    label: 'Please provide OCR criteria',
+    onClose: () => {
+      // do nothing.
+    },
+    components,
+  });
 };
