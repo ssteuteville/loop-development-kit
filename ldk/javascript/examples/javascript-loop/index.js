e3ea2de05dd2259853bf880dba76cc3b9a6eb155
Preston Scibek <preston@bytelion.com>
2021-10-13 11:14:12 -0400

    Adding functional OCR loop for completion of spike

--- ldk/javascript/examples/javascript-loop/index.js
+++ ldk/javascript/examples/javascript-loop/index.js
@@ -1,10 +1,35 @@
-import { screen } from '@oliveai/ldk';
+import { screen, whisper } from '@oliveai/ldk';
 import { BoundingBoxType } from '@oliveai/ldk/dist/screen';
+import { write } from '../../dist/clipboard';
 
-function clipboardListenAndWhisper() {
-  screen.ocr(BoundingBoxType.Block).then((result) => {
-    console.log(`result: ${result}`);
+
+const writeWhisper = (labelV, body) => {
+  whisper.create({
+    label: labelV,
+    onClose: () => {
+      console.log(`Closed Whisper`);
+    },
+    components: [
+      {
+        body,
+        type: whisper.WhisperComponentType.Markdown,
+      },
+    ],
+  });
+};
+
+
+function OcrLoop() {
+  writeWhisper(`ocr`, `starting ocr app`)
+  screen.ocr(0).then((result) => {
+    console.log("OCR Results: ");
+    console.log(JSON.stringify(result));
+    writeWhisper(`result`, `${JSON.stringify(result)}`)
+  }).catch((error) => {
+    console.log("error: ");
+    console.log(error);
   });
 }
 
-clipboardListenAndWhisper();
+OcrLoop();
+console.log(`starting app`)
